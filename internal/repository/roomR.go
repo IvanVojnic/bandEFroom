6c04ca7436555ecd261cf2f55a63ca460b96adca
IvanVojnic <ivojnic44@gmail.com>
2023-03-02 00:39:04 +0300

    feat: get rooms test

--- internal/repository/roomR.go
+++ internal/repository/roomR.go
@@ -22,41 +22,35 @@ func NewRoomPostgres(db *pgxpool.Pool) *RoomPostgres {
 func (r *RoomPostgres) GetRooms(ctx context.Context, userID uuid.UUID) ([]models.Room, error) {
 	var rooms []models.Room
 	rowsUserRooms, errUserRooms := r.db.Query(ctx,
-		`SELECT ROOMS.id, ROOMS.room_idUserCreator, ROOMS.room_place, ROOMS.room_date, USERS.email, STATUSES.status FROM ROOMS
-				INNER JOIN INVITES on INVITES.room_id = ROOMS.id AND INVITES.user_id = $1
-				INNER JOIN USERS on USERS.id = ROOMS.room_idUserCreator
-				INNER JOIN statuses on STATUSES.id = INVITES.status_id`, userID)
+		`SELECT ROOMS.id, ROOMS.idUserCreator, ROOMS.place, ROOMS.date FROM ROOMS
+			 INNER JOIN INVITES on INVITES.room_id = ROOMS.id AND INVITES.user_id = $1`, userID)
 	if errUserRooms != nil {
 		return rooms, fmt.Errorf("error while getting invites")
 	}
 	defer rowsUserRooms.Close()
 	for rowsUserRooms.Next() {
 		var room models.Room
-		errScan := rowsUserRooms.Scan(&room.RoomID, &room.RoomUserID, &room.RoomPlace, &room.RoomDate)
+		errScan := rowsUserRooms.Scan(&room.ID, &room.UserCreatorID, &room.Place, &room.Date)
 		if errScan != nil {
 			return rooms, fmt.Errorf("get all friends requests scan rows error %w", errScan)
 		}
 		rooms = append(rooms, room)
 	}
-	for i := 0; i < len(rooms); i++ {
-		var users []models.User
-		rowsUsers, errUsers := r.db.Query(ctx,
-			`SELECT users.id, users.email, invites.status_id FROM users
-				INNER JOIN invites on invites.user_id = users.id
-				WHERE invites.room_id=$1`, rooms[i].RoomID)
-		if errUsers != nil {
-			return rooms, fmt.Errorf("error while getting invited users")
+	var invites []models.Invite
+	rowsUserInvites, errUserInvites := r.db.Query(ctx,
+		`SELECT ROOMS.id, ROOMS.idUserCreator, ROOMS.place, ROOMS.date FROM ROOMS
+			 INNER JOIN INVITES on INVITES.room_id = ROOMS.id AND INVITES.user_id = $1`, userID)
+	if errUserInvites != nil {
+		return rooms, fmt.Errorf("error while getting invites")
+	}
+	defer rowsUserInvites.Close()
+	for rowsUserInvites.Next() {
+		var invite models.Invite
+		errScan := rowsUserInvites.Scan()
+		if errScan != nil {
+			return rooms, fmt.Errorf("get all friends requests scan rows error %w", errScan)
 		}
-		defer rowsUsers.Close()
-		for rowsUsers.Next() {
-			var user models.User
-			errScan := rowsUserRooms.Scan(&user.UserID, &user.UserEmail)
-			if errScan != nil {
-				return rooms, fmt.Errorf("get all friends requests scan rows error %w", errScan)
-			}
-			users = append(users, user)
-		}
-		rooms[i].Users = users
+		invites = append(rooms, invite)
 	}
 	return rooms, nil
 }
--- migrations/sql/V1__initial_setup_create_events.sql
+++ migrations/sql/V1__initial_setup_create_events.sql
@@ -2,9 +2,7 @@ create table if not exists rooms(
     id uuid primary key not null,
     idUserCreator uuid not null,
     place varchar(255) not null,
-    date timestamp not null,
-    FOREIGN KEY (idUserCreator) REFERENCES users(id)
-        ON DELETE CASCADE
+    date timestamp not null
 );
 
 create table if not exists invites(
@@ -12,8 +10,6 @@ create table if not exists invites(
     user_id uuid not null,
     room_id uuid not null,
     status int not null,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-      ON DELETE CASCADE,
     FOREIGN KEY (room_id) REFERENCES rooms(id)
       ON DELETE CASCADE
 );
