130d0d0a2c405f2ef16c1c46b4a307e8ffcc48a3
IvanVojnic <ivojnic44@gmail.com>
2023-03-01 13:14:24 +0300

    feat: get rooms started pack

--- internal/rpc/roomRPC.go
+++ internal/rpc/roomRPC.go
@@ -1,1 +1,51 @@
 package rpc
+
+import (
+	"context"
+	"fmt"
+	"github.com/IvanVojnic/bandEFroom/models"
+	pr "github.com/IvanVojnic/bandEFroom/proto"
+	"github.com/sirupsen/logrus"
+
+	"github.com/google/uuid"
+)
+
+type Room interface {
+	GetRooms(ctx context.Context, user uuid.UUID) ([]models.Room, error)
+}
+
+type RoomServer struct {
+	pr.UnimplementedRoomServer
+	roomServ Room
+}
+
+func NewRoomServer(roomServ Room) *RoomServer {
+	return &RoomServer{roomServ: roomServ}
+}
+
+func (s *RoomServer) DeclineInvite(ctx context.Context, req *pr.DeclineInviteRequest) (*pr.DeclineInviteResponse, error) {
+	userID, errUserParse := uuid.Parse(req.GetUserID())
+	if errUserParse != nil {
+		logrus.WithFields(logrus.Fields{
+			"Error parse user ID": errUserParse,
+			"userID":              userID,
+		}).Errorf("error parsing ID, %s", errUserParse)
+		return &pr.DeclineInviteResponse{}, fmt.Errorf("error while parsing ID, %s", errUserParse)
+	}
+	roomID, errRoomParse := uuid.Parse(req.GetRoomID())
+	if errRoomParse != nil {
+		logrus.WithFields(logrus.Fields{
+			"Error parse room ID": errRoomParse,
+			"roomID":              roomID,
+		}).Errorf("error parsing ID, %s", errRoomParse)
+		return &pr.DeclineInviteResponse{}, fmt.Errorf("error while parsing ID, %s", errRoomParse)
+	}
+	errDecline := s.inviteServ.AcceptInvite(ctx, userID, roomID, int(req.GetStatusID()))
+	if errDecline != nil {
+		logrus.WithFields(logrus.Fields{
+			"Error decline invite": errDecline,
+		}).Errorf("error decline invite, %s", errDecline)
+		return &pr.DeclineInviteResponse{}, fmt.Errorf("error while decling invite, %s", errDecline)
+	}
+	return &pr.DeclineInviteResponse{}, nil
+}
--- models/Rooms.go
+++ models/Rooms.go
@@ -1,1 +1,14 @@
 package models
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+// Room is a Room
+type Room struct {
+	ID            uuid.UUID `json:"id" db:"id"`
+	UserCreatorID uuid.UUID `json:"userCreatorID" db:"idUserCreator"`
+	Place         string    `json:"place" db:"place"`
+	Date          time.Time `json:"date" db:"date"`
+}
